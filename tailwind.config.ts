e7263f4905eb0cf0ce078c99e809faeb06e688a2
Santiago Garcia <just.b000ringsh1t@gmail.com>
2024-09-08 18:23:17 -0300

    feat: :tada: first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,14 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
